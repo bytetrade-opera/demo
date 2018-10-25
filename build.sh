2d3b8a78ca73b8dab9b3dd03aa34927e7da28a5b
solomon <you@example.com>
2018-10-25 19:01:28 +0800

    update

--- build.sh
+++ build.sh
@@ -5,4 +5,18 @@ if [[ ! -f a ]]; then
 fi
 cd -
 
+if [[ ! -f librdkafka-0.11.3.tar.gz ]]; then
+    wget https://s3-ap-northeast-1.amazonaws.com/ccd1/librdkafka-0.11.3.tar.gz -O librdkafka-0.11.3.tar.gz
+    tar -xzvf librdkafka-0.11.3.tar.gz
+    cd librdkafka-0.11.3
+    ./configure
+    make -j$JOBS
+    sudo make install
+    cd -
+else
+    cd librdkafka-0.11.3
+    sudo make install
+    cd -
+fi
+
 g++ t.cpp 
